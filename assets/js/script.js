4386e9c7d0f6ae09ab4035f44b55a13f18c474a2
trife631 <126350708+trife631@users.noreply.github.com>
2023-03-21 17:14:12 -0400

    added javascript for bing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var searchForm = document.querySelector('#search-form')
 var qInput = document.querySelector('#q');
+var videosList = document.getElementById('videos');
 
-var handleSearch = function (event) {
+var wikiHandleSearch = function (event) {
     event.preventDefault();
     var q = qInput.value.trim();
     
@@ -14,8 +15,7 @@ var handleSearch = function (event) {
 
    fetch(apiURL)
    .then(function (response) {
-                console.log(response)
-                return response.json();
+            return response.json();
             })
             .then(function (data) {
                 console.log(data);
@@ -25,4 +25,25 @@ var handleSearch = function (event) {
             });
 }
  
-searchForm.addEventListener('submit', handleSearch);
+searchForm.addEventListener('submit', wikiHandleSearch);
+
+var bingOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '168a4d3a4cmsh9a1eadacaa150cap199683jsn69b0a6c3e172',
+        'X-RapidAPI-Host': 'bing-video-search1.p.rapidapi.com'
+    }
+};
+
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var q = qInput.value.trim();
+    fetch(`https://bing-video-search1.p.rapidapi.com/videos/search?count=5&q=${q}`, bingOptions)
+        .then(response => response.json())
+        .then(response => {
+            videosList.innerHTML = '';
+            var videos = response.value;
+            console.log(videos)       
+        })
+        .catch(err => console.error(err));
+});
